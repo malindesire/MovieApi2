24923d59e41be91d5892bfd2c8a4f977a4f309c9
malindesire <malindesirejohansson@gmail.com>
2025-07-07 16:08:58 +0200

    Add relation to Review and add migration

--- MovieApi/Models/Entities/Movie.cs
+++ MovieApi/Models/Entities/Movie.cs
@@ -8,5 +8,6 @@
         public string Genre { get; set; } = null!;
         public int Duration { get; set; } // Duration in minutes
         public MovieDetails MovieDetails { get; set; } = null!;
+        public ICollection<Review> Reviews { get; set; } = [];
     }
 }
--- MovieApi/Models/Entities/Review.cs
+++ MovieApi/Models/Entities/Review.cs
@@ -6,5 +6,7 @@
         public string ReviewerName { get; set; } = null!;
         public string Comment { get; set; } = null!;
         public int Rating { get; set; } // Rating between 1 and 5
+        public int MovieId { get; set; }
+        public Movie Movie { get; set; } = null!; // Navigation property to the Movie entity
     }
 }
