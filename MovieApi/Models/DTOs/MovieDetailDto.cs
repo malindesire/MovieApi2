fead99aefe5ce04e1e3014fee1c0b68e3337f25b
malindesire <malindesirejohansson@gmail.com>
2025-07-09 00:18:03 +0200

    Add MovieDetailDto

---
+++ MovieApi/Models/DTOs/MovieDetailDto.cs
@@ -0,0 +1,9 @@
+﻿namespace MovieApi.Models.DTOs
+{
+    public record MovieDetailDto : MovieDto
+    {
+        public required string Synopsis { get; init; } = string.Empty;
+        public List<ActorDto> Actors { get; init; } = new List<ActorDto>();
+        public List<ReviewDto> Reviews { get; init; } = new List<ReviewDto>();
+    }
+}
