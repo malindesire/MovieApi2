11ab61679ec550071e006d50f4f89a7e5e1b390a
malindesire <malindesirejohansson@gmail.com>
2025-07-09 14:16:39 +0200

    Flatten MovieDetailDto

--- MovieApi/Models/DTOs/MovieDetailDto.cs
+++ MovieApi/Models/DTOs/MovieDetailDto.cs
@@ -1,8 +1,16 @@
 ﻿namespace MovieApi.Models.DTOs
 {
-    public record MovieDetailDto : MovieDto
+    public record MovieDetailDto
     {
+        public required int Id { get; init; }
+        public required string Title { get; init; }
+        public required int Year { get; init; }
+        public required string Genre { get; init; }
+        public required int Duration { get; init; }
+        public required string Language { get; init; }
+        public required decimal Budget { get; init; }
         public required string Synopsis { get; init; } = string.Empty;
+        public required double AverageRating { get; init; }
         public List<ActorDto> Actors { get; init; } = new List<ActorDto>();
         public List<ReviewDto> Reviews { get; init; } = new List<ReviewDto>();
     }
