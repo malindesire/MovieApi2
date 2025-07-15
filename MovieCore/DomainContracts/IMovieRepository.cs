6b8dda886bb761f2f1d9a407d109c3d112750816
malindesire <malindesirejohansson@gmail.com>
2025-07-15 14:31:19 +0200

    Add interface IMovieRepository

---
+++ MovieCore/DomainContracts/IMovieRepository.cs
@@ -0,0 +1,14 @@
+﻿using MovieCore.Models.Entities;
+
+namespace MovieCore.DomainContracts
+{
+    public  interface IMovieRepository
+    {
+        Task<IEnumerable<Movie>> GetAllAsync();
+        Task<Movie?> GetAsync(int id);
+        Task<bool> AnyAsync(int id);
+        void Add(Movie movie);
+        void Update(Movie movie);
+        void Remove(Movie movie);
+    }
+}
