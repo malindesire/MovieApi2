ec6560f5789abbe2b5d2d8c45e007a8a52f6576a
malindesire <malindesirejohansson@gmail.com>
2025-07-15 16:23:49 +0200

    Add UnitOfWork

---
+++ MovieData/Repositories/UnitOfWork.cs
@@ -0,0 +1,24 @@
+﻿using MovieCore.DomainContracts;
+using MovieData.Data;
+
+namespace MovieData.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly MovieContext _context;
+
+        public IMovieRepository Movies { get; }
+        public IActorRepository Actors { get; }
+        public IReviewRepository Reviews { get; }
+
+        public UnitOfWork(MovieContext context)
+        {
+            _context = context;
+            Movies = new MovieRepository(context);
+            Actors = new ActorRepository(context);
+            Reviews = new ReviewRepository(context);
+        }
+
+        public async Task CompleteAsync() => await _context.SaveChangesAsync();
+    }
+}
