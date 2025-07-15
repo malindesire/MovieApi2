b6f87cfcf3cb5323d425f642d18352d66f27a583
malindesire <malindesirejohansson@gmail.com>
2025-07-15 15:32:36 +0200

    Add ReviewRepository

---
+++ MovieData/Repositories/ReviewRepository.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieCore.Models.Entities;
+using MovieData.Data;
+
+namespace MovieData.Repositories
+{
+    public class ReviewRepository
+    {
+        protected DbSet<Review> DbSet { get; }
+        public ReviewRepository(MovieContext context)
+        {
+            DbSet = context.Set<Review>();
+        }
+        public async Task<IEnumerable<Review>> GetAllAsync(int movieId) => await DbSet.Where(r => r.MovieId == movieId).ToListAsync();
+    }
+}
