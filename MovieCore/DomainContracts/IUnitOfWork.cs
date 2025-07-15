c54e4b654967b7184ce724d2d1eeaf27becdec4b
malindesire <malindesirejohansson@gmail.com>
2025-07-15 16:23:37 +0200

    Add interface IUnitOfWork

---
+++ MovieCore/DomainContracts/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿namespace MovieCore.DomainContracts
+{
+    public interface IUnitOfWork
+    {
+        IMovieRepository Movies { get; }
+        IActorRepository Actors { get; }
+        IReviewRepository Reviews { get; }
+        Task CompleteAsync();
+    }
+}
