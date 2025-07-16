5416db5ba5d845afd788543be7d155ec0d0bcadb
malindesire <malindesirejohansson@gmail.com>
2025-07-16 17:19:43 +0200

    Implement UOW in ActorsController

--- MovieApi/Controllers/ActorsController.cs
+++ MovieApi/Controllers/ActorsController.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using MovieData.Data;
+using MovieCore.DomainContracts;
 
 namespace MovieApi.Controllers
 {
@@ -8,53 +7,22 @@ namespace MovieApi.Controllers
     [ApiController]
     public class ActorsController : ControllerBase
     {
-        private readonly MovieContext _context;
+        private readonly IUnitOfWork _uow;
 
-        public ActorsController(MovieContext context)
+        public ActorsController(IUnitOfWork unitOfWork)
         {
-            _context = context;
+            _uow = unitOfWork;
         }
 
         // POST: api/Movies/5/Actors/3
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost("/api/movies/{movieId}/actors/{actorId}")]
         public async Task<ActionResult> AddActorToMovie(int movieId, int actorId)
-        {   
-            var movie = await _context.Movies.
-                Include(m => m.Actors).
-                FirstOrDefaultAsync(m => m.Id == movieId);
-
-            if (movie == null)
-            {
-                return NotFound($"Movie with ID {movieId} not found.");
-            }
-
-            var actor = await _context.Actors.FirstOrDefaultAsync(a => a.Id == actorId);
-            if (actor == null)
-            {
-                return NotFound($"Actor with ID {actorId} not found.");
-            }
-
-            // Check if the actor is already associated with the movie
-            if (movie.Actors.Any(a => a.Id == actorId))
-            {
-                return BadRequest($"Actor with ID {actorId} is already associated with Movie ID {movieId}.");
-            }
-
-            // Add the actor to the movie's actors collection
-            movie.Actors.Add(actor);
-
-            _context.Entry(movie).State = EntityState.Modified;
-            _context.Entry(actor).State = EntityState.Unchanged; // Ensure the actor's state remains unchanged
-
-            await _context.SaveChangesAsync();
-
-            return Ok();
-        }
-
-        private bool ActorExists(int id)
         {
-            return _context.Actors.Any(e => e.Id == id);
+            _uow.Actors.AddActorToMovie(movieId, actorId);
+            await _uow.CompleteAsync();
+
+            return Ok($"Actor with ID {actorId} is added to movie with ID {movieId}");
         }
     }
 }
