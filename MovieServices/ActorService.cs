82653fa94d203a38e8018e2be8b45e66d3ef1911
malindesire <malindesirejohansson@gmail.com>
2025-07-17 16:37:50 +0200

    Add ActorService with methods

---
+++ MovieServices/ActorService.cs
@@ -0,0 +1,21 @@
+﻿using MovieCore.DomainContracts;
+using MovieServiceContracts;
+
+namespace MovieServices
+{
+    public class ActorService : IActorService
+    {
+        private IUnitOfWork _uow;
+
+        public ActorService(IUnitOfWork uow)
+        {
+            this._uow = uow;
+        }
+
+        public async Task AddActorToMovieAsync(int movieId, int actorId)
+        {
+            _uow.Actors.AddActorToMovie(movieId, actorId);
+            await _uow.CompleteAsync();
+        }
+    }
+}
