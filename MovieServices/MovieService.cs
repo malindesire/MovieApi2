8ed059d827ef443e73e1cfb3564277c4c997a879
malindesire <malindesirejohansson@gmail.com>
2025-07-17 14:23:21 +0200

    Implement methods in MovieService

--- MovieServices/MovieService.cs
+++ MovieServices/MovieService.cs
@@ -1,5 +1,6 @@
 ﻿using MovieCore.DomainContracts;
 using MovieCore.Models.DTOs;
+using MovieCore.Models.Entities;
 using MovieServiceContracts;
 
 namespace MovieServices
@@ -40,29 +41,138 @@ namespace MovieServices
             });
         }
 
-        public Task<MovieDto> GetMovieAsync(int id, bool includeActors)
+        public async Task<MovieDto> GetMovieAsync(int id, bool includeActors)
         {
-            throw new NotImplementedException();
+            var movie = await _uow.Movies.GetAsync(id, includeActors);
+
+            if (movie == null)
+            {
+                return null;
+            }
+
+            return new MovieDto
+            {
+                Id = movie.Id,
+                Title = movie.Title,
+                Year = movie.Year,
+                Genre = movie.Genre,
+                Duration = movie.Duration,
+                Actors = includeActors ? movie.Actors
+                    .Select(a => new ActorDto
+                        {
+                            Id = a.Id,
+                            FullName = $"{a.FirstName} {a.LastName}",
+                            BirthYear = a.BirthYear
+                        })
+                    .ToList() : null
+            };
         }
 
-        public Task<MovieDetailDto> GetMovieWithDetails(int id)
+        public async Task<MovieDetailDto> GetMovieWithDetails(int id)
         {
-            throw new NotImplementedException();
+            var movie = await _uow.Movies.GetWithDetailsAsync(id);
+
+            if (movie == null)
+            {
+                return null;
+            }
+
+            return new MovieDetailDto
+            {
+                Id = movie.Id,
+                Title = movie.Title,
+                Year = movie.Year,
+                Genre = movie.Genre,
+                Duration = movie.Duration,
+                Language = movie.MovieDetails.Language,
+                Budget = movie.MovieDetails.Budget,
+                Synopsis = movie.MovieDetails.Synopsis,
+                AverageRating = movie.Reviews.Any() ? movie.Reviews.Average(r => r.Rating) : 0.0,
+                Reviews = movie.Reviews
+                    .Select(r => new ReviewDto
+                        {
+                            Id = r.Id,
+                            Rating = r.Rating,
+                            Comment = r.Comment,
+                            ReviewerName = r.ReviewerName
+                        })
+                    .ToList(),
+                Actors = movie.Actors
+                    .Select(a => new ActorDto
+                        {
+                            Id = a.Id,
+                            FullName = $"{a.FirstName} {a.LastName}",
+                            BirthYear = a.BirthYear
+                        })
+                    .ToList()
+            };
+
         }
 
-        public Task UpdateMovie(int id, MovieUpdateDto dto)
+        public async Task UpdateMovie(int id, MovieUpdateDto dto)
         {
-            throw new NotImplementedException();
+            var movie = await _uow.Movies.GetWithDetailsAsync(id);
+
+            if (movie == null)
+            {
+                throw new KeyNotFoundException($"Movie with ID {id} not found.");
+            }
+
+            // Update the movie properties from the DTO
+            movie.Title = dto.Title;
+            movie.Year = dto.Year;
+            movie.Genre = dto.Genre;
+            movie.Duration = dto.Duration;
+            movie.MovieDetails.Synopsis = dto.Synopsis;
+            movie.MovieDetails.Language = dto.Language;
+            movie.MovieDetails.Budget = dto.Budget;
+
+            _uow.Movies.Update(movie);
+            await _uow.CompleteAsync();
         }
 
-        public Task<MovieDetailDto> AddMovie(MovieCreateDto dto)
+        public async Task<MovieDetailDto> AddMovie(MovieCreateDto dto)
         {
-            throw new NotImplementedException();
+            var movie = new Movie 
+            {
+                Title = dto.Title,
+                Year = dto.Year,
+                Genre = dto.Genre,
+                Duration = dto.Duration,
+                MovieDetails = new MovieDetails
+                {
+                    Language = dto.Language,
+                    Budget = dto.Budget,
+                    Synopsis = dto.Synopsis
+                }
+            };
+
+            _uow.Movies.Add(movie);
+            await _uow.CompleteAsync();
+
+            return new MovieDetailDto
+            {
+                Id = movie.Id,
+                Title = movie.Title,
+                Year = movie.Year,
+                Genre = movie.Genre,
+                Duration = movie.Duration,
+                Language = movie.MovieDetails.Language,
+                Budget = movie.MovieDetails.Budget,
+                Synopsis = movie.MovieDetails.Synopsis,
+            };
         }
 
-        public Task RemoveMovie(int id)
+        public async Task RemoveMovie(int id)
         {
-            throw new NotImplementedException();
+            var movie = await _uow.Movies.GetAsync(id, false);
+            if (movie == null)
+            {
+                throw new KeyNotFoundException($"Movie with ID {id} not found.");
+            }
+
+            _uow.Movies.Remove(movie);
+            await _uow.CompleteAsync();
         }
     }
 }
