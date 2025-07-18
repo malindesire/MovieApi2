473971b0c35744c612ee8d8ec7bd59f99ba988ee
malindesire <malindesirejohansson@gmail.com>
2025-07-18 14:53:11 +0200

    Add class that handles the metadata for pagination

---
+++ MovieCore/Requests/PaginationMetadata.cs
@@ -0,0 +1,19 @@
+﻿namespace MovieCore.Requests
+{
+    public class PaginationMetadata
+    {
+        public int PageSize { get; set; }
+        public int CurrentPage { get; set; }
+        public int TotalItemCount { get; set; }
+        public int TotalPageCount { get; set; }
+
+
+        public PaginationMetadata(int pageSize, int currentPage, int totalItemCount)
+        {
+            PageSize = pageSize;
+            CurrentPage = currentPage;
+            TotalItemCount = totalItemCount;
+            TotalPageCount = (int)Math.Ceiling(totalItemCount / (double)pageSize);
+        }
+    }
+}
